b0dbab41fb143fa31004097d1ff6c056e022814e
ognenini <59257605+ognenini@users.noreply.github.com>
2020-04-26 18:29:45 +0200

    Add files via upload

---
+++ morse.py
@@ -0,0 +1,406 @@
+# -*- coding: utf-8 -*-
+import sys
+import playsound
+import time
+
+
+print(r"""
+
+                                   
+           
+     __  __  ___  ____  ____  _____   _____ ____      _    _   _ ____  _        _  _____ ___  ____  
+    |  \/  |/ _ \|  _ \/ ___|| ____| |_   _|  _ \    / \  | \ | / ___|| |      / \|_   _/ _ \|  _ \ 
+    | |\/| | | | | |_) \___ \|  _|     | | | |_) |  / _ \ |  \| \___ \| |     / _ \ | || | | | |_) |
+    | |  | | |_| |  _ < ___) | |___    | | |  _ <  / ___ \| |\  |___) | |___ / ___ \| || |_| |  _ < 
+    |_|  |_|\___/|_| \_\____/|_____|   |_| |_| \_\/_/   \_\_| \_|____/|_____/_/   \_\_| \___/|_| \_\
+                                                                                                 
+   
+     -- --- .-. ... .    - .-. .- -. ... .-.. .- - --- .-. 
+                                                                      
+""")
+
+
+
+
+                                       
+                                  
+                                                                                  
+
+
+
+
+
+
+
+
+def split(rec):
+    return [char for char in rec]
+
+def lo():
+    playsound.playsound('long.mp3')
+    
+
+
+def sh():
+    playsound.playsound('short.mp3')
+
+
+    
+a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z=0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+prompt = ""
+ 
+
+def a():
+    sh()
+    lo()
+    time.sleep(0.7)
+
+
+def b():
+    lo()    
+    sh()    
+    sh()    
+    sh()
+    time.sleep(0.7)
+
+
+def c():
+    lo()
+    sh()
+    lo()
+    sh()
+    time.sleep(0.7)
+
+def d():
+    playsound.playsound('long.mp3')
+    playsound.playsound('short.mp3')
+    playsound.playsound('short.mp3')
+    time.sleep(0.7)
+
+def e():
+    playsound.playsound('short.mp3')
+    time.sleep(0.7)
+
+def f():
+    playsound.playsound('short.mp3')
+    playsound.playsound('short.mp3')
+    playsound.playsound('long.mp3')
+    playsound.playsound('short.mp3')
+    time.sleep(0.7)
+
+def g():
+    lo()
+    lo()
+    sh()
+    time.sleep(0.7)
+
+def h():
+    sh()
+    sh()
+    sh()
+    sh()
+    time.sleep(0.7)
+
+
+
+def j():
+    sh()
+    lo()
+    lo()
+    lo()
+    time.sleep(0.7)
+
+def k():
+    lo()
+    sh()
+    lo()
+    time.sleep(0.7)
+
+def l():
+    sh()
+    lo()
+    sh()
+    sh()
+    time.sleep(0.7)
+
+def m():
+    lo()
+    lo()
+    time.sleep(0.7)
+
+
+
+def o():
+    lo()
+    lo()
+    lo()
+    time.sleep(0.7)
+
+def p():
+    sh()
+    lo()
+    lo()
+    sh()
+    time.sleep(0.7)
+
+def q():
+    lo()
+    lo()
+    sh()
+    lo()
+    time.sleep(0.7)
+
+def r():
+    sh()
+    lo()
+    sh()
+    time.sleep(0.7)
+
+def s():
+    sh()
+    sh()
+    sh()
+    time.sleep(0.7)
+
+def t():
+    lo()
+    time.sleep(0.7)
+
+def u():
+    sh()
+    sh()
+    lo()
+    time.sleep(0.7)
+
+def v():
+    sh()
+    sh()
+    sh()
+    lo()
+    time.sleep(0.7)
+
+def w():
+    sh()
+    lo()
+    lo()
+    time.sleep(0.7)
+
+def x():
+    lo()
+    sh()
+    sh()
+    lo()
+    time.sleep(0.7)
+
+def y():
+    lo()
+    sh()
+    lo()
+    lo()
+    time.sleep(0.7)
+
+def z():
+    lo()
+    lo()
+    sh()
+    sh()
+    time.sleep(0.7)
+
+def jedan():
+    sh()
+    lo()
+    lo()
+    lo()
+    lo()
+    time.sleep(0.7)
+
+def dva():
+    sh()
+    sh()
+    lo()
+    lo()
+    lo()
+    time.sleep(0.7)
+
+def tri():
+    sh()
+    sh()
+    sh()
+    lo()
+    lo()
+    time.sleep(0.7)
+
+def cetiri():
+    sh()
+    sh()
+    sh()
+    sh()
+    lo()
+    time.sleep(0.7)
+
+def pet():
+    sh()
+    sh()
+    sh()
+    sh()
+    sh()
+    time.sleep(0.7)
+
+def sest():
+    lo()
+    sh()
+    sh()
+    sh()
+    sh()
+    time.sleep(0.7)
+
+def sedam():
+    lo()
+    lo()
+    sh()
+    sh()
+    sh()
+    time.sleep(0.7)
+
+def osam():
+    lo()
+    lo()
+    lo()
+    sh()
+    sh()
+    time.sleep(0.7)
+
+def devet():
+    lo()
+    lo()
+    lo()
+    lo()
+    sh()
+    time.sleep(0.7)
+
+def nula():
+    lo()
+    lo()
+    lo()
+    lo()
+    lo()
+    time.sleep(0.7)
+
+def raz():
+    time.sleep(1.2)
+
+while True:
+    rec=input("Type the sentence:")
+    niz=split(rec)
+
+
+
+    n=0
+    i=len(niz)
+
+
+
+    while n < i:
+
+        if niz[n] in {'a', 'A'}:
+            a()
+        elif niz[n] in {'b', 'B'}:
+            b()
+        elif niz[n] in {'c', 'C'}:
+            c()
+        elif niz[n] in {'d', 'D'}:
+            d()
+        elif niz[n] in {'e', 'E'}:
+            e()
+        elif niz[n] in {'f', 'F'}:
+            f()
+        elif niz[n] in {'g', 'G'}:
+            g()
+        elif niz[n] in {'h', 'H'}:
+            h()
+        elif niz[n] in {'i', 'I'}:
+            #i()
+            sh()
+            sh()
+
+            time.sleep(0.7)
+        elif niz[n] in {'j', 'J'}:
+            j()
+        elif niz[n] in {'k', 'K'}:
+            k()
+        elif niz[n] in {'l', 'L'}:
+            l()
+        elif niz[n] in {'m', 'M'}:
+            m()
+        elif niz[n] in {'n', 'N'}:
+            #n()
+            lo()
+            sh()
+            time.sleep(0.7)
+        elif niz[n] in {'o', 'O'}:
+            o()
+        elif niz[n] in {'p', 'P'}:
+            p()
+        elif niz[n] in {'q', 'Q'}:
+            q()
+        elif niz[n] in {'r', 'R'}:
+            r()
+        elif niz[n] in {'s', 'S'}:
+            s()
+        elif niz[n] in {'t', 'T'}:
+            t()
+        elif niz[n] in {'u', 'U'}:
+            u()
+        elif niz[n] in {'v', 'V'}:
+            v()
+        elif niz[n] in {'w', 'W'}:
+            w()
+        elif niz[n] in {'x', 'X'}:
+            x()
+        elif niz[n] in {'y', 'Y'}:
+            y()
+        elif niz[n] in {'z', 'Z'}:
+            z()
+        elif niz[n] in {' '}:
+            raz()
+        elif niz[n] in {'1'}:
+            jedan()
+        elif niz[n] in {'2'}:
+            dva()
+        elif niz[n] in {'3'}:
+            tri()
+        elif niz[n] in {'4'}:
+            cetiri()
+        elif niz[n] in {'5'}:
+            pet()
+        elif niz[n] in {'6'}:
+            sest()
+        elif niz[n] in {'7'}:
+            sedam()
+        elif niz[n] in {'8'}:
+            osam()
+        elif niz[n] in {'9'}:
+            devet()
+        elif niz[n] in {'0'}:
+            nula()
+
+
+    
+    
+        n=n+1
+    prompt = input("Another translation?(y/n) > ")
+    if prompt == "n":
+        sys.exit()
+    elif prompt == "y":
+        continue
+        
+    while prompt != "n" and prompt != "y":
+        
+        print("Invalid option!")
+        prompt = input("Another translation?(y/n) > ")
+        if prompt == "n":
+            print("Good bye!")
+            sys.exit()
+        elif prompt == "y":
+            continue
+
